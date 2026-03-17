e213491fbddbd27865bc41eb6b08fe34c117cc4f
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-18 01:07:29 +0500

    Update ExternalLogisticsServiceB.java

--- src/adapter/ExternalLogisticsServiceB.java
+++ src/adapter/ExternalLogisticsServiceB.java
@@ -1,4 +1,12 @@
-package adapter;
+package org.example.adapter;
 
 public class ExternalLogisticsServiceB {
+
+    public void sendPackage(String info) {
+        System.out.println("ServiceB sending package " + info);
+    }
+
+    public String checkPackageStatus(String code) {
+        return "ServiceB: Delivered";
+    }
 }
