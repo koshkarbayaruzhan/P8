d120b2d4bd91509e6abd89ef5ecbcdaccd077c00
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-18 01:07:51 +0500

    Update LogisticsAdapterA.java

--- src/adapter/LogisticsAdapterA.java
+++ src/adapter/LogisticsAdapterA.java
@@ -1,4 +1,14 @@
-package adapter;
+package org.example.adapter;
 
-public class LogisticsAdapterA {
+public class LogisticsAdapterA implements IInternalDeliveryService {
+
+    private ExternalLogisticsServiceA service = new ExternalLogisticsServiceA();
+
+    public void deliverOrder(String orderId) {
+        service.shipItem(Integer.parseInt(orderId));
+    }
+
+    public String getDeliveryStatus(String orderId) {
+        return service.trackShipment(Integer.parseInt(orderId));
+    }
 }
