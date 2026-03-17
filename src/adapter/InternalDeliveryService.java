3b52ced0e896cbd49e37b5705bfd6cc83d1216cf
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-18 01:06:36 +0500

    Update InternalDeliveryService.java

--- src/adapter/InternalDeliveryService.java
+++ src/adapter/InternalDeliveryService.java
@@ -1,4 +1,12 @@
-package adapter;
+package org.example.adapter;
 
-public class InternalDeliveryService {
+public class InternalDeliveryService implements IInternalDeliveryService {
+
+    public void deliverOrder(String orderId) {
+        System.out.println("Internal delivery for order " + orderId);
+    }
+
+    public String getDeliveryStatus(String orderId) {
+        return "Internal: Delivered";
+    }
 }
