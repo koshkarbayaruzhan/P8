5554fae34c2756fe56f48860ca92ef52c7619c9d
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-18 01:07:10 +0500

    Update ExternalLogisticsServiceA.java

--- src/adapter/ExternalLogisticsServiceA.java
+++ src/adapter/ExternalLogisticsServiceA.java
@@ -1,4 +1,12 @@
-package adapter;
+package org.example.adapter;
 
 public class ExternalLogisticsServiceA {
+
+    public void shipItem(int itemId) {
+        System.out.println("ServiceA shipping item " + itemId);
+    }
+
+    public String trackShipment(int shipmentId) {
+        return "ServiceA: In transit";
+    }
 }
